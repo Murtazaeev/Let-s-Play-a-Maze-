7f7dedb37ad7fe37d600c40ae2a113dc9d7b84e0
Temur <74430488+Murtazayev@users.noreply.github.com>
2021-01-30 15:28:01 +0100

    Add files via upload

---
+++ wall.js
@@ -0,0 +1,7 @@
+const walls = [
+	Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+	Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+	Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+	Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
+];
+World.add(world, walls);
