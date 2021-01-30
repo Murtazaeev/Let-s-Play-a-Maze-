58538722deccfe6a6d7985c4cb16fb75606548b4
Temur <74430488+Murtazayev@users.noreply.github.com>
2021-01-30 15:29:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 4;
-const cellsVertical = 3;
+const cellsHorizontal = 14;
+const cellsVertical = 10;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
